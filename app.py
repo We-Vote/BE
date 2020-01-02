ce7554b8629706fe0a89128781af519920d05386
DaftBeowulf <ajmboosh@gmail.com>
2020-01-02 14:10:38 -0600

    :construction: Sanity check for testing environments

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
+# app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -14,4 +15,3 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
-    print(os.environ['APP_SETTINGS'])
